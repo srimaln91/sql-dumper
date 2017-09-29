4428083af34f53d8d4d7f3f400d2e88402607290
H.M.S.Nishantha <srimal@3cs.lk>
2017-09-29 19:49:37 +0530

    Added cmd argument to specify backup directory

--- src/Commands/RestoreDatabase.php
+++ src/Commands/RestoreDatabase.php
@@ -6,6 +6,7 @@ use Pimple\Container;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use DBDump\Database\Connection;
 use DBDump\Database\RestoreService;
 use DBDump\Lib\FileSystem;
@@ -44,7 +45,8 @@ class RestoreDatabase extends Command
     {
         $this->setName('db:restore')
         ->setDescription("Restore a database dump")
-        ->setHelp("This command allows you to restore a database dump");
+        ->setHelp("This command allows you to restore a database dump")
+        ->addArgument('db_dir', InputArgument::REQUIRED, 'Please tell me where your db backup files stored?');
     }
 
 
@@ -57,8 +59,8 @@ class RestoreDatabase extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-
-        if (FileSystem::isDirEmpty($this->config->get('directory.db'))) {
+        $dbBackupDir = $input->getArgument('db_dir');
+        if (FileSystem::isDirEmpty($dbBackupDir)) {
 
             $output->writeln("<error>Dump directory is empty!</error>");
             return;
@@ -67,7 +69,7 @@ class RestoreDatabase extends Command
         $restoreService = new RestoreService(
             $this->connection,
             $this->config->get('binaries'),
-            $this->config->get('directory.db')
+            $dbBackupDir
         );
 
         try {
